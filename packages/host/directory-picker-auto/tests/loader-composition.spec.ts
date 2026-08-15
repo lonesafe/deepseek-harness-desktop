a28a5da9a02c815f1f726f21697cd70c3f22bf56
roubsite <85195433@qq.com>
2026-08-16 01:54:33 +0800

    test(host): supply token for all-interface composition

--- packages/host/directory-picker-auto/tests/loader-composition.spec.ts
+++ packages/host/directory-picker-auto/tests/loader-composition.spec.ts
@@ -99,6 +99,7 @@ async function loadComposition(
     '  config:',
     `    host: '${bindHost}'`,
     '    port: 0',
+    "    accessToken: 'a-secure-24-character-token'",
     `- name: '${AUTO}'`,
     '',
   ].join('\n'))
